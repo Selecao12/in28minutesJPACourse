35af28fe273a3cac53644f1efb7afab6b960051f
vitalik <witalikalive@gmail.com>
2021-11-02 00:37:34 +0500

    step 39 - Retrieve Data using JPA Relationships - finished

--- src/test/java/com/in28minutes/jpa/hibernate/demo/repository/StudentRepositoryTests.java
+++ src/test/java/com/in28minutes/jpa/hibernate/demo/repository/StudentRepositoryTests.java
@@ -42,4 +42,12 @@ class StudentRepositoryTests {
         logger.info("passport -> {}", passport);
         logger.info("student -> {}", passport.getStudent());
     }
+
+    @Test
+    @Transactional
+    public void retrieveStudentAndCourses() {
+        Student student = em.find(Student.class, 20001L);
+        logger.info("student -> {}", student);
+        logger.info("courses -> {}", student.getCourses());
+    }
 }
