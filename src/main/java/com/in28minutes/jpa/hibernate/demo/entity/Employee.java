f626a3002f6c649d5f735bc463dbeb0a5c6f28c4
vitalik <witalikalive@gmail.com>
2021-11-07 01:14:45 +0500

    step 43 - JPA Inheritance Hierarchies and Mappings - Setting up entities - finished

---
+++ src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Employee.java
@@ -0,0 +1,44 @@
+package com.in28minutes.jpa.hibernate.demo.entity;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public abstract class Employee {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    protected Employee() {
+    }
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Employee[%s]", name);
+    }
+}
