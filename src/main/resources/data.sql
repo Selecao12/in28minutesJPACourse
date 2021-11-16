0fcf6980e3a1e92786e35a037c859d4371d7c2a2
vitalik <witalikalive@gmail.com>
2021-11-17 00:00:17 +0500

    step 55, 56 - JPQL - Criteria Query - Courses like 100 Steps, without Students - finished

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
 insert into course(id, name, created_date, last_updated_date)
-values (10001, 'JPA in 50 steps', sysdate(), sysdate());
+values (10001, 'JPA in 50 Steps', sysdate(), sysdate());
 
 insert into course(id, name, created_date, last_updated_date)
-values (10002, 'Spring in 50 steps', sysdate(), sysdate());
+values (10002, 'Spring in 50 Steps', sysdate(), sysdate());
 
 insert into course(id, name, created_date, last_updated_date)
-values (10003, 'Spring Boot in 50 steps', sysdate(), sysdate());
+values (10003, 'Spring Boot in 100 Steps', sysdate(), sysdate());
 
 insert into passport (id, number)
 values (40001, 'E123456');
