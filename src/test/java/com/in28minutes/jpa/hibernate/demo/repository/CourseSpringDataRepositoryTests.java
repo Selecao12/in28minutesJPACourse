da93a88dc0351c89a041907719494aa89454a652
vitalik <witalikalive@gmail.com>
2021-11-30 01:29:29 +0500

    Step 68 - Pagination using Spring Data JPA Repository - finished

--- src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CourseSpringDataRepositoryTests.java
+++ src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CourseSpringDataRepositoryTests.java
@@ -8,6 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.DirtiesContext;
 
@@ -52,17 +55,23 @@ class CourseSpringDataRepositoryTests {
 
     @Test
     public void sort() {
-//        Course course = repository.save(new Course("Microservices in 100 Steps"));
-//        repository.save(course);
-
-//        course.setName("Microservices in 100 Steps - Updated");
-//        repository.save(course);
 
         Sort sort = Sort.by(Sort.Direction.DESC, "name");
         logger.info("Sorted Courses -> {}", repository.findAll(sort));
 //        [Course[JPA in 50 Steps], Course[Spring in 50 Steps], Course[Spring Boot in 100 Steps]]
 //        [Course[Spring in 50 Steps], Course[Spring Boot in 100 Steps], Course[JPA in 50 Steps]]
         logger.info("Count -> {}", repository.count());
+    }
+
+    @Test
+    public void pagination() {
+        PageRequest pageRequest = PageRequest.of(0, 3);
+        Page<Course> firstPage = repository.findAll(pageRequest);
+        logger.info("First Page -> {}", firstPage.getContent());
+
+        Pageable secondPageable = firstPage.nextPageable();
+        Page<Course> secondPage = repository.findAll(secondPageable);
+        logger.info("Second Page -> {}", secondPage.getContent());
     }
 
 }
