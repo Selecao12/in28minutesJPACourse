985360103a66a7c480550aef80552c5116ec5f31
vitalik <witalikalive@gmail.com>
2021-11-30 01:46:11 +0500

    Step 70 - Spring Data REST - finished

--- src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Course.java
+++ src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Course.java
@@ -1,5 +1,6 @@
 package com.in28minutes.jpa.hibernate.demo.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -26,6 +27,7 @@ public class Course {
     private List<Review> reviews = new ArrayList<>();
 
     @ManyToMany(mappedBy = "courses")
+    @JsonIgnore
     private List<Student> students = new ArrayList<>();
 
     @UpdateTimestamp
--- src/main/java/com/in28minutes/jpa/hibernate/demo/repository/CourseSpringDataRepository.java
+++ src/main/java/com/in28minutes/jpa/hibernate/demo/repository/CourseSpringDataRepository.java
@@ -3,9 +3,11 @@ package com.in28minutes.jpa.hibernate.demo.repository;
 import com.in28minutes.jpa.hibernate.demo.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(path = "courses")
 public interface CourseSpringDataRepository extends JpaRepository<Course, Long> {
 
     List<Course> findByNameAndId(String name, Long id);
