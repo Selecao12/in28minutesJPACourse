0cb65a3c90e9a39ad73263df64ad164ea8530aed
vitalik <witalikalive@gmail.com>
2021-11-30 01:06:04 +0500

    Step 67 - Sorting using Spring Data JPA Repository - finished

--- src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CourseSpringDataRepositoryTests.java
+++ src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CourseSpringDataRepositoryTests.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.DirtiesContext;
 
 import javax.persistence.EntityManager;
@@ -46,6 +47,21 @@ class CourseSpringDataRepositoryTests {
 //        repository.save(course);
 
         logger.info("Courses -> {}", repository.findAll());
+        logger.info("Count -> {}", repository.count());
+    }
+
+    @Test
+    public void sort() {
+//        Course course = repository.save(new Course("Microservices in 100 Steps"));
+//        repository.save(course);
+
+//        course.setName("Microservices in 100 Steps - Updated");
+//        repository.save(course);
+
+        Sort sort = Sort.by(Sort.Direction.DESC, "name");
+        logger.info("Sorted Courses -> {}", repository.findAll(sort));
+//        [Course[JPA in 50 Steps], Course[Spring in 50 Steps], Course[Spring Boot in 100 Steps]]
+//        [Course[Spring in 50 Steps], Course[Spring Boot in 100 Steps], Course[JPA in 50 Steps]]
         logger.info("Count -> {}", repository.count());
     }
 
